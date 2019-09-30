33f878bc96e2fdbfd91c3dcc3fbfc3398d46893b
troyAmlee <troyamlee19@gmail.com>
2019-09-30 17:27:26 -0500

    added pseudocode to assignment 2 question 3

--- Assignment_02/score_group4_question3.cpp
+++ Assignment_02/score_group4_question3.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
+/*
+ * Variables required:
+ * first_score, 
+ * second_score, 
+ * third_score, 
+ * highest, 
+ * lowest: (floating point) to store the first score, second score, third score,
+ * 		highest score out of the three, and lowest score out of the three
+ * 
+ * Pseudocode solution:
+ * Display "Please enter three exam scores: "
+ * Read the first score, second score, and third score into the
+ * 		variables first_score, second_score, and third_score
+ * if first_score >= second_score
+ * 		highest = first_score
+ * 		lowest = second_score
+ * else
+ * 		highest = second_score
+ * 		lowest = first_score
+ * if third_score >= highest
+ * 		highest = third_score
+ * if third_score >= lowest
+ * 		Display "Highest score = " and highest
+ * 		Display "Lowest score = " and lowest
+ * else
+ * 		lowest = third_score
+ * 		Display "Highest score = " and highest
+ * 		Display "Lowest score = " and lowest
+*/
 
 int main(){
 	double first_score;
