c8966db8227eac46ccffe5a091d7e068534d4176
troyAmlee <troyamlee19@gmail.com>
2019-10-08 16:01:21 -0500

    started question 3

--- assignment_03/assign3q3.cpp
+++ assignment_03/assign3q3.cpp
@@ -5,5 +5,15 @@
 using namespace std;
 
 int main(){
-    
+    char option;
+    cout << "Please enter an option." 
+         << endl
+         << "(B)alance  (D)eposit   (W)ithdraw  e(X)it"
+         << endl;
+    switch(option){
+        case 'b' || 'B':
+            cout << "Test";
+            break;
+    }
+    return 0;
 }
