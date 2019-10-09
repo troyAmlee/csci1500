a9535486f7000df3aba4402dae9677a251c3007a
troyAmlee <troyamlee19@gmail.com>
2019-10-09 15:42:57 -0500

    Question 4 c++ code is complete.

--- assignment_03/assign3q4.cpp
+++ assignment_03/assign3q4.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+double hypot(double a, double b){
+    double csquared = pow(a,2) + pow(b,2);
+    double c = sqrt(csquared);
+
+    return c;
+}
+
 int main(){
-    
+    double sidea, sideb;
+    cout << "Enter the side lengths of a triangle: ";
+    cin >> sidea >> sideb;
+    cout << "First side length = " << sidea << endl;
+    cout << "Second side length = " << sideb << endl;
+    cout << "Hypotenuse length = " << hypot(sidea, sideb) << endl;
+
+    return 0;
 }
