7ecdadff884adfef97c7e8e6779a13dffc671f0f
troyAmlee <troyamlee19@gmail.com>
2019-10-09 17:20:02 -0500

    Completed question 6 c++ code.

--- assignment_03/assign3q6.cpp
+++ assignment_03/assign3q6.cpp
@@ -4,6 +4,24 @@
 
 using namespace std;
 
+int y;
+
+void leapYear(int year){
+    if(year%400 == 0 || year%4 == 0 && year%100 != 0){
+        cout << year
+             << " is a leap year"
+             << endl;
+    }
+    else{
+        cout << year
+             << " is not a leap year"
+             << endl;
+    }
+}
+
 int main(){
-    
+    cout << "Enter year: ";
+    cin >> y;
+    leapYear(y);
+    return 0;
 }
