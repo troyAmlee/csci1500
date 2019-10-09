7e7a9ee4e53f4744ff4814a642731f3e6f1286ff
troyAmlee <troyamlee19@gmail.com>
2019-10-09 18:23:00 -0500

    Completed c++ code for question 7

--- assignment_03/assign3q7.cpp
+++ assignment_03/assign3q7.cpp
@@ -4,6 +4,34 @@
 
 using namespace std;
 
+int getWeight(){
+    float package;
+    cout << "Enter package weight(oz): ";
+    cin >> package;
+    return package;
+}
+
+float deliveryCharge(float weight){
+    int multiple = ((weight - 16)/4);
+    float additionalcharge = multiple*0.50;
+    float totalcharge = 3.00 + additionalcharge;
+    return totalcharge;
+}
+
+void displayCharge(){
+    int ounces = getWeight();
+    cout.setf(ios::showpoint);
+    cout.setf(ios::fixed);
+    cout.precision(2);    cout << "Package weight = " 
+         << ounces/16
+         << " lb. " 
+         << ounces%16
+         << " oz. "
+         << endl
+         << "Delivery charge = $"
+         << deliveryCharge(ounces);
+}
+
 int main(){
-    
+    displayCharge();
 }
