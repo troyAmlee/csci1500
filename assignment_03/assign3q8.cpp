ec046f6f53e2d498c7d288636614ee30a5d4b61c
troyAmlee <troyamlee19@gmail.com>
2019-10-10 13:23:37 -0500

    Almost to a solution for quest8

--- assignment_03/assign3q8.cpp
+++ assignment_03/assign3q8.cpp
@@ -4,6 +4,31 @@
 
 using namespace std;
 
+void divisors(int num){
+    
+    for(int i = 2; i < num; i++){
+        if(num%i == 0){
+
+        }
+    }
+}
+
 int main(){
+    int someint;
+    char option;
+    cout << "Enter a number: ";
+    cin >> someint;
+    do{
+        divisors(someint);
+        cout << "Continue? (y or n): ";
+        cin >> option;
+        if(option == 'y'){
+            continue;
+        }
+        else{
+            break;
+        }
+    }while(cin >> someint);
+        
     
 }
