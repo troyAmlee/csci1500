a23bdd25def55a69d05ddcd5dee10726a00a900f
troyAmlee <troyamlee19@gmail.com>
2019-10-10 15:18:35 -0500

    Finished question 8 c++ code

--- assignment_03/assign3q8.cpp
+++ assignment_03/assign3q8.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
 #include <string>
+#include <array>
 #include <cmath>
 
 using namespace std;
 
 void divisors(int num){
-    
+    int count = 0;
+    double totalsumdiv = 0;
+    array<int,100> array1;
     for(int i = 2; i < num; i++){
         if(num%i == 0){
-
+            array1[count] = i;
+            count++;
         }
     }
+    cout << "Proper divisors: ";
+    for(int j = 0; j < count; j++){
+        cout << " " << array1[j];
+        totalsumdiv = totalsumdiv + array1[j];
+    }
+    cout << "\nNumber of proper divisors = " 
+         << count
+         << endl
+         << "Sum of proper divisors = "
+         << totalsumdiv
+         << endl;
 }
 
 int main(){
     int someint;
     char option;
     cout << "Enter a number: ";
-    cin >> someint;
-    do{
+    cin >> someint; 
+    do{       
         divisors(someint);
         cout << "Continue? (y or n): ";
         cin >> option;
         if(option == 'y'){
+            cout << "Enter a number: ";
             continue;
         }
         else{
