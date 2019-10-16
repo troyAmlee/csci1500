d5ed506f84ac53f74dd16c68bcbb960654998029
troyAmlee <troyamlee19@gmail.com>
2019-10-16 18:06:27 -0500

    modified question 8 to hold more space.

--- assignment_03/assign3q8.cpp
+++ assignment_03/assign3q8.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void divisors(int num){
     int count = 0;
     double totalsumdiv = 0;
-    int array1 [100000];
+    double array1 [200000];
     for(int i = 2; i < num; i++){
         if(num%i == 0){
             array1[count] = i;
