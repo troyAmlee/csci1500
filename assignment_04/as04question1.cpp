14ce538cf8448d7f600844fd275ec75ffaf8aa63
troyAmlee <troyamlee19@gmail.com>
2019-10-29 14:43:19 -0500

    phu t. did question 1

--- assignment_04/as04question1.cpp
+++ assignment_04/as04question1.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 using namespace std;
 
+void time(int seconds, int & hours,int & min,int & sec ){
+	
+	
+ hours = seconds / 3600;
+ min = (seconds % 3600) / 60;
+ sec = seconds % 60;
+	
+	
+	
+	
+}
+
 int main(){
+	
+	int seconds, hours, min, sec;
+	
+	cout << "Enter a time in seconds :";
+	
+	cin >> seconds;
+	
+	
+	time (seconds,hours,min,sec);
+	
+	cout << seconds << "seconds = " 
+	     << hours << " hour (s)"
+	     << min << "minute (s)" 
+		 << sec << "second (s)";
 	
 	
 	return 0;
 }
+
