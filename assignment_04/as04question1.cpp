330a566b554637d30dc9d643212d788d6e7c72f8
troyAmlee <troyamlee19@gmail.com>
2019-10-29 15:23:48 -0500

    Done with question 2 c++ code

--- assignment_04/as04question1.cpp
+++ assignment_04/as04question1.cpp
@@ -3,13 +3,9 @@ using namespace std;
 
 void time(int seconds, int & hours,int & min,int & sec ){
 	
-	
  hours = seconds / 3600;
  min = (seconds % 3600) / 60;
  sec = seconds % 60;
-	
-	
-	
 	
 }
 
@@ -17,18 +13,16 @@ int main(){
 	
 	int seconds, hours, min, sec;
 	
-	cout << "Enter a time in seconds :";
+	cout << "Enter a time in seconds : ";
 	
 	cin >> seconds;
-	
 	
 	time (seconds,hours,min,sec);
 	
 	cout << seconds << "seconds = " 
-	     << hours << " hour (s)"
-	     << min << "minute (s)" 
-		 << sec << "second (s)";
-	
+	     << hours << " hour(s)"
+	     << min << " minute(s)" 
+		 << sec << " second(s)";
 	
 	return 0;
 }
--- assignment_04/as04question2.cpp
+++ assignment_04/as04question2.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+const double pi = 3.141592654;
+
+double calc(double rad, double hei, double& area, double& volume);
+
 int main(){
+	double radius = 0;
+	double height = 0;
+	double areaFinal = 0;
+	double volumeFinal = 0;
 	
+	
+	cout << "Please enter can radius (inches): ";
+	cin >> radius;
+	cout << "Please enter can height (inches): ";
+	cin >> height;
+		
+	calc(radius, height, areaFinal, volumeFinal);
+	cout << "Radius: " 
+		 << radius 
+		 << endl 
+		 << "Height: " 
+		 << height 
+		 << endl
+		 << "Can surface area = "
+		 << areaFinal
+		 << " square inches "
+		 << endl
+		 << "Can volume = "
+		 << volumeFinal
+		 << " cubic inches"
+		 << endl;
 	
 	return 0;
+}
+
+double calc(double rad, double hei, double& area, double& volume){
+	volume = pi*pow(rad, 2)*hei;
+	area = 2*pi*rad*hei + 2*pi*pow(rad, 2);
 }
