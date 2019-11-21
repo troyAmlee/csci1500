201c1174cc6f3dd599859dbd72664cf11d9cacb6
troyAmlee <troyamlee19@gmail.com>
2019-11-21 17:10:10 -0600

    Added comments to explain recursion action

--- assignment_06/assign6_q1.cpp
+++ assignment_06/assign6_q1.cpp
@@ -14,7 +14,7 @@ int main(){
 		cin >> x;
 		cout << "Enter an integer number n: ";
 		cin >> expo;
-		cout << "The value of x^n = " << recPower(x, expo) << endl;
+		cout << "The value of x^n = " << recPower(x, expo) << endl; // if x is 5 and expo is 3: line 36-44
 		cout << "Continue (y or n)? ";
 		cin >> option;
 	}while(option == 'y' || option == 'Y');
@@ -23,7 +23,8 @@ int main(){
 }
 
 long double recPower(long double digit, int exponent){
-	//cout << "Exponent Status: " << exponent << endl;
+	cout << "Exponent Status: " << exponent << endl;
+	cout << "Digit Status: " << digit << endl;
 	
 	if(exponent < 0){
 		return (recPower(digit, exponent + 1)*(1.0/digit));
@@ -31,6 +32,17 @@ long double recPower(long double digit, int exponent){
 	
 	if(exponent > 0){
 		return recPower(digit, exponent - 1)*digit;	
+		/*
+		recPower(5, 3)
+			recPower(5, 3 - 1) * digit <-- suspended
+				recPower(5, 2 - 1) * digit <-- suspended
+					recPower(5, 1 - 1) * digit <-- suspended
+						recPower(5, 0) == 1 <-- returns 1
+							so now...
+								recPower(5, 1 - 1) * digit == 1 * 5 == 5 == recPower(5, 1)
+									recPower(5, 2 - 1) * digit == 5 * 5 == 25 == recPower(5, 2)
+										recPower(5, 3 - 1) * digit == 25 * 5 == 125 == recPower(5, 3)					
+		*/
 	}
 	
 	else{
