4e0ecca16af1f3bc1130cb7b9695e65238835938
troyAmlee <troyamlee19@gmail.com>
2019-12-03 14:50:01 -0600

    Added lab

--- freestyleCode/lab12032019.cpp
+++ freestyleCode/lab12032019.cpp
@@ -6,19 +6,16 @@ using namespace std;
 
 int main (){
 	
-	
 	int num [8];
 	int sum = 0;
 	double average = 0;
 	
 	for (int i = 0; i < 8; i ++){
 		
-		
 		cin >> num[i];
 		
 		sum+= num[i];
 	}
-	
 	
 	average = sum/8.0;
 	
@@ -26,14 +23,9 @@ int main (){
 	
 	for (int i = 0 ; i < 8; i ++){
 		
-		
 		cout << num[i] << setw(7) << num[i] - average << endl;
 		
-		
-		
 	}
-	
-	
 	
 	return 0;
 }
