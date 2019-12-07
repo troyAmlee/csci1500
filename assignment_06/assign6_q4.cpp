932cc4e867a233eb18bf7247520e4d55d29ab2bb
troyAmlee <troyamlee19@gmail.com>
2019-12-07 00:54:39 -0600

    Completed question 4

---
+++ assignment_06/assign6_q4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main(){
+	string sentence = "";
+	string word;
+	int wordCount = 0;
+	string longestWord = "";
+	double wordSum = 0;
+	do{
+		cout << "Enter a sentence: ";
+		cin >> word;
+		
+		if((word[0] != ',' && word[0] != ';' && word[0] != '.' && (word[0] != '!') && word[0] != '?')){
+			wordCount++;
+			wordSum = wordSum + word.length();	
+		}
+		
+		if(word.length() >= longestWord.length()){
+			longestWord = word;
+		}
+		sentence = sentence + word + " ";
+		cout << word[0] << endl;
+	}while(!((word[0] == '.') || (word[0] == '!') || (word[0] == '?')));
+	cout << sentence << endl;
+	cout << "Number of words = " << wordCount << endl;
+	cout << "Average word length = " << wordSum/wordCount << endl;
+	cout << "Longest word: " << longestWord << endl;
+	
+	return 0;
+}
